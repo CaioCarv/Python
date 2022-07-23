f7981d80774dbecdab6068a97760e1cd1bdc9588
Perkles <caiocfvc@gmail.com>
2022-07-23 14:53:38 -0300

    Mais exercicios

---
+++ ex036.py
@@ -0,0 +1,12 @@
+casa = float(input('QUal o valor da casa?'))
+salario = float(input('Qual é o seu salário?'))
+anos = float(input('Em quantos anos o Sr. deseja pagar?'))
+
+prestacao = casa / anos
+limite = salario * 0.3
+
+if prestacao < limite:
+    print('O salário do Sr. é {} e as parcelas serão {},'
+          ' então está autorizado o seu empréstimo'.format(salario, prestacao))
+else:
+    print('O empréstimo do Sr. foi negado, pois a parcela de {}, está muito alta para o Senhor.'.format(prestacao))
