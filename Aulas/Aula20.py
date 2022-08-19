d5bd656fb243bee479dbd19ed4573ea083443cb3
Perkles <caiocfvc@gmail.com>
2022-08-18 22:31:50 -0300

    Create Aula20.py

---
+++ Aulas/Aula20.py
@@ -0,0 +1,20 @@
+def lin():
+    print('-' * 20)
+
+
+def titulo(txt):
+    lin()
+    print(txt)
+
+
+def soma(a, b):
+    r = a + b
+    print(r)
+
+
+titulo(' CURSO EM VIDEO ')
+titulo(' Aprenda Python ')
+titulo('  Caio Felipe  ')
+soma(5, 4)
+
+
